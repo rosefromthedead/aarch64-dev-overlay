f827088f84426a07e2af8e118e3412bf28310957
Tommy Hudson <thomhuds@protonmail.com>
2021-06-07 09:45:30 +0100

    Update hash again, maybe due to cargo-vendor change

--- mkarm64image.nix
+++ mkarm64image.nix
@@ -6,13 +6,13 @@ rustPlatform.buildRustPackage {
   pname = "mkarm64image";
   version = "0.1.0";
   src = fetchFromGitHub {
-    owner = "thomhuds";
+    owner = "rosehuds";
     repo = "mkarm64image";
     rev = "f896ea50b3ba9b71aaed117898653008aa797ad5";
     sha256 = "1xxdb03a0c8clxikigjx9pcafg47zx7sylnr5vak7qb71yar8i9q";
   };
   
-  cargoSha256 = "1qmb75d9391hcjqm0vcl5933lks3wphyiv4k4w85wlrah638c5j8";
+  cargoSha256 = "0kri0vv2m09rnwywh1dr2bhg1z5bkpxrvcswlawkyb19dqfh46xm";
   verifyCargoDeps = true;
 }
 
